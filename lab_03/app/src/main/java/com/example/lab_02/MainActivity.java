ee150b4eaae1742a01acb6a2c4dcad8e34e88ba2
Bohdan Polishchuk <bohdan.polishchuk2000@gmail.com>
2021-04-08 16:24:44 +0300

    no message

---
+++ lab_03/app/src/main/java/com/example/lab_02/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.lab_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+
+
+    private static final String TAG = "MainActivity";
+    DatabaseHelper mDatabaseHelper;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
+    }
+
+
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view){
+        EditText input = findViewById(R.id.editText2);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+
+            boolean insertData = mDatabaseHelper.addData(itemText);
+            if(insertData) {Toast.makeText(getApplicationContext(), "Data Successfully Saved!", Toast.LENGTH_SHORT).show();}
+            else {Toast.makeText(getApplicationContext(), "Something's wrong, I can feel it...", Toast.LENGTH_SHORT).show();}
+
+            input.setText("");
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Please enter text", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
